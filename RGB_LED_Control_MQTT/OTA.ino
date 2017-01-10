7d5d44e73280c9dd88a43270c8ff5626c3e29783
Kaushik Ray <ray.1_1@live.com>
2017-01-09 21:38:24 -0500

    removed some useless code

--- RGB_LED_Control_MQTT/OTA.ino
+++ RGB_LED_Control_MQTT/OTA.ino
@@ -77,23 +77,3 @@ void manage_housekeeping()
   if (WiFi.status() != WL_CONNECTED)    manage_WiFi();
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
-/*
-void setup_wifi() {
-
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}*/
